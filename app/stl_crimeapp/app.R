42dbc77e04f13121cf3f43cc360ed81fcefe2c41
laknath123 <laknathdias@gmail.com>
2021-03-05 18:53:06 -0500

    Plot 1 complete
    Plot 1 completed

--- app/stl_crimeapp/app.R
+++ app/stl_crimeapp/app.R
@@ -3,9 +3,11 @@ library(shinydashboard)
 library(data.table)
 library(dplyr)
 library(stringr)
+library(plotly)
+
 
 # Loading dataset 
-df <- read.csv("policedata.csv", nrows = 10000)
+df <- read.csv("violent_crimes.csv")
 df_murder <- df %>% filter(description=="HOMICIDE")
 df_car <- df %>% filter(str_detect(description, "^ROBBERY CARJACKING"))
 
@@ -20,6 +22,21 @@ header <- dashboardHeader(title = "St.Louis Crime")
 
 # Dashboard Sidebar ----------------------------------------------
 sidebar <- dashboardSidebar(
+  sidebarMenu(
+    id = "tabs",
+    
+    # Menu Items ----------------------------------------------
+    menuItem("Plot", icon = icon("bar-chart"), tabName = "plot"),
+  
+  # Inputs: select variables to plot ----------------------------------------------
+    selectInput("monthSelect",
+              "Months:",
+              choices = sort(unique(df$Month)),
+              multiple = TRUE,
+              selectize = TRUE,
+              selected = c("Jan", "Feb")),
+  
+  
         sliderInput("timeofday",
                     "Time (24-hour clock)",
                     min = min(df$timeofday,na.rm = T),
@@ -27,16 +44,31 @@ sidebar <- dashboardSidebar(
                     value = c(min(df$timeofday,na.rm = T),max(df$timeofday,na.rm = T)),
                     step =1)      
 
-)
+))
 
 
 # Dashboard body ----------------------------------------------
-body <- dashboardBody(
+body <- dashboardBody(tabItems(
+  tabItem("plot",
           # Value Boxes ----------------------------------------------
+          fluidRow(
             infoBoxOutput("info_box1"),
             infoBoxOutput("info_box2"),
             infoBoxOutput("info_box3")
-)
+          ),
+          
+          
+          # Plot ----------------------------------------------
+          fluidRow(
+            tabBox(title = "Plot",
+                   width = 12,
+                   tabPanel("Crime Type", plotlyOutput("plot_type")),
+                   tabPanel("Crime In Parks", plotlyOutput("plot_park")))
+          
+          )
+)))
+
+
 
 
 
@@ -49,7 +81,13 @@ server <- function(input, output) {
   crimedatainput <- reactive({
     crimedf <- df %>%
       filter(timeofday >= input$timeofday[1] & timeofday<= input$timeofday[2])
+      # Month Filter ----------------------------------------------
+    if (length(input$monthSelect) > 0 ) {
+      crimedf <- subset(crimedf, Month %in% input$monthSelect)
+    }
     
+    # Return dataframe ----------------------------------------------
+    return(crimedf)
   })
   
   homdatainput <- reactive({
@@ -62,7 +100,17 @@ server <- function(input, output) {
       filter(timeofday >= input$timeofday[1] & timeofday<= input$timeofday[2])
   })
   
+  # Plots -------------------------------------------------------------
+  # A plot showing crimetypes
+  output$plot_type <- renderPlotly({
+    dat <- crimedatainput()
+    ggplot(data = dat)+geom_bar(aes(x = UCR_casetype, y = stat(prop),group=1))
+  })
   
+  
+  
+  
+
   # Mass mean info box ----------------------------------------------
   output$info_box1 <- renderInfoBox({
     cd <- crimedatainput()
